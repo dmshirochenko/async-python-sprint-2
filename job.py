72b86b235700c096f493d754dabd51243f2c55ec
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-01-03 22:10:52 +0100

    initial commit

--- job.py
+++ job.py
@@ -1,12 +1,25 @@
+from functools import wraps
+from typing import Callable
+
+def coroutine(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        gen = f(*args, **kwargs)
+        gen.send(None)
+        return gen
+
+    return wrap
+
 class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
-        pass
+    def __init__(
+        self, 
+        target: Callable, 
+        args: tuple | None = None, 
+        kwargs: dict | None = None,
+    ) -> None:
+        self.__args = args or ()
+        self.__kwargs = kwargs or {}
+        self.__coroutine = target(*self.__args, **self.__kwargs)
 
-    def run(self):
-        pass
-
-    def pause(self):
-        pass
-
-    def stop(self):
-        pass
+    def run(self) -> None:
+        self.__coroutine.send(None)
--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,19 @@
+from queue import Queue
+
+from job import Job
+
 class Scheduler:
-    def __init__(self, pool_size=10):
-        pass
+    def __init__(self) -> None:
+        self.__queue: Queue[Job] = Queue()
 
-    def schedule(self, task):
-        pass
+    def add_job(self, task: Job) -> None:
+        self.__queue.put(task)
 
-    def run(self):
-        pass
-
-    def restart(self):
-        pass
-
-    def stop(self):
-        pass
+    def run(self) -> None:
+        while not self.__queue.empty():
+            job = self.__queue.get()
+            try:
+                job.run()
+            except StopIteration:
+                continue
+            self.add_job(job)
