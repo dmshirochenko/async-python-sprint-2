b31eec84a3a01ef0edb9810f055df1f8338ead3c
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-01-06 00:36:23 +0100

    adding tasks for scheduler check

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import time
 import uuid
+import os
 from typing import Generator
 
 from job import Job, coroutine
 from scheduler import Scheduler
+from utils import FileSystemOperations, NetworkOperationsPipe
 
 
 @coroutine
 def print_x(num: int, name: str) -> Generator[None, None, None]:
     for i in range(num):
         yield
-        time.sleep(1)
+        time.sleep(0.1)
         print(f"Вызов функции print_x с именем {name}")
 
 
 if __name__ == "__main__":
-    job1 = Job(func=print_x, job_id=uuid.uuid4(), args=(1, "Один"))
-    job2 = Job(func=print_x, job_id=uuid.uuid4(), args=(2, "Два"))
-    job3 = Job(func=print_x, job_id=uuid.uuid4(), args=(3, "Три"), dependencies=[job1, job2])
+    dir_name = "output_news_folder"
+    file_name = "yahoo_news.txt"
+    link = (
+        "https://news.yahoo.com/trump-legal-news-brief-supreme-court-agrees-to-"
+        "quickly-decide-whether-trump-can-be-kept-off-ballots-223044557.html"
+    )
+    path = os.path.join(dir_name, file_name)
+
+    job1 = Job(func=FileSystemOperations().create_directory, job_id=uuid.uuid4(), args=[dir_name])
+    job2 = Job(func=FileSystemOperations().create_file, job_id=uuid.uuid4(), args=[path])
+    job3 = Job(
+        func=NetworkOperationsPipe().html_to_txt_pipeline, job_id=uuid.uuid4(), args=[link, path]
+    )  # dependencies=[job1, job2])
 
     loop = Scheduler()
     loop.schedule(job1)
--- utils.py
+++ utils.py
@@ -1,28 +1,151 @@
 import os
+import shutil
 import requests
+import logging.config
+from html.parser import HTMLParser
+
+from config.logger import LOGGING
+from job import coroutine
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
-def file_system_operations(job_id, _):
-    print(f"{job_id}: Performing file system operations")
-    os.makedirs("test_dir", exist_ok=True)
-    with open("test_dir/test_file.txt", "w") as file:
-        file.write("Hello, World!")
-    os.remove("test_dir/test_file.txt")
-    os.rmdir("test_dir")
+class FileSystemOperations:
+    @staticmethod
+    @coroutine
+    def create_directory(path):
+        try:
+            os.makedirs(path)
+            yield f"Directory created at {path}"
+        except FileExistsError:
+            yield f"Directory exists at {path}"
+
+    @staticmethod
+    @coroutine
+    def delete_directory(path):
+        try:
+            shutil.rmtree(path)
+            yield f"Directory deleted at {path}"
+        except FileNotFoundError:
+            yield "Directory not found"
+
+    @staticmethod
+    @coroutine
+    def create_file(path):
+        try:
+            with open(path, "w") as file:
+                file.write("")
+            yield f"File created at {path}"
+        except Exception as e:
+            yield f"Error creating file at {path}: {e}"
+
+    @staticmethod
+    @coroutine
+    def delete_file(path):
+        try:
+            os.remove(path)
+            yield f"File deleted at {path}"
+        except FileNotFoundError:
+            yield "File not found"
 
 
-def file_operations(job_id, _):
-    print(f"{job_id}: Performing file operations")
-    with open("example.txt", "w") as file:
-        file.write("Sample text")
-    with open("example.txt", "r") as file:
-        content = file.read()
-    print(f"{job_id}: File content: {content}")
-    os.remove("example.txt")
+class FileOperations:
+    @staticmethod
+    @coroutine
+    def write_to_file(path, content):
+        with open(path, "w") as file:
+            file.write(content)
+        yield f"Content written to {path}"
+
+    @staticmethod
+    @coroutine
+    def read_from_file(path):
+        try:
+            with open(path, "r") as file:
+                for line in file:
+                    yield line
+        except FileNotFoundError:
+            yield "File not found"
 
 
-def network_operations(job_id, _):
-    print(f"{job_id}: Performing network operations")
-    url = "https://www.example.com"
-    response = requests.get(url)
-    print(f"{job_id}: Response status code: {response.status_code}")
+class ChunkHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.text_parts = []
+        self.ignore_data = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag in ("script", "style"):
+            self.ignore_data = True
+
+    def handle_endtag(self, tag):
+        if tag in ("script", "style"):
+            self.ignore_data = False
+
+    def handle_data(self, data):
+        if not self.ignore_data:
+            self.text_parts.append(data)
+
+    def handle_entityref(self, name):
+        self.text_parts.append(self.unescape("&" + name + ";"))
+
+    def handle_charref(self, name):
+        self.text_parts.append(self.unescape("&#" + name + ";"))
+
+    def get_data(self):
+        return " ".join(part.strip() for part in self.text_parts if part.strip())
+
+
+class NetworkOperationsPipe:
+    @staticmethod
+    @coroutine
+    def html_to_txt_pipeline(url, path):
+        output = NetworkOperationsPipe().clean_html_chunks()
+        output.send(path)
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+            for chunk in response.iter_content(chunk_size=None):
+                if chunk:
+                    yield output.send(chunk.decode("utf-8"))
+        except requests.exceptions.RequestException as e:
+            raise e
+
+    @staticmethod
+    @coroutine
+    def write_to_file():
+        path = yield
+        try:
+            with open(path, "w") as file:
+                while True:
+                    chunk = yield
+                    file.write(chunk)
+                    file.flush()
+        except IOError as e:
+            logger.error(f"IOError while writing to file {path}: {e}")
+            raise e
+        except GeneratorExit:
+            logger.info(f"Finished writing to {path}")
+
+    @staticmethod
+    @coroutine
+    def clean_html_chunks():
+        output = NetworkOperationsPipe.write_to_file()
+        parser = ChunkHTMLParser()
+        path = yield
+        output.send(path)
+        try:
+            while True:
+                chunk = yield
+                parser.feed(chunk)
+                parsed_text = parser.get_data()
+                output.send(parsed_text)
+        except UnicodeDecodeError as e:
+            logger.error(f"UnicodeDecodeError while parsing HTML: {e}")
+            raise e
+        except GeneratorExit:
+            logger.info("Html document has been parsed correctly")
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            raise e
