00ca652c91701fc93dbbf4fde8939e525fc35b84
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-01-05 01:13:46 +0100

    adding working version of scheduler with dependencies

--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
 import os
 import requests
 
+
 def file_system_operations(job_id, _):
     print(f"{job_id}: Performing file system operations")
     os.makedirs("test_dir", exist_ok=True)
     with open("test_dir/test_file.txt", "w") as file:
         file.write("Hello, World!")
     os.remove("test_dir/test_file.txt")
-    os.rmdir("test_dir") 
+    os.rmdir("test_dir")
+
 
 def file_operations(job_id, _):
     print(f"{job_id}: Performing file operations")
@@ -17,6 +19,7 @@ def file_operations(job_id, _):
         content = file.read()
     print(f"{job_id}: File content: {content}")
     os.remove("example.txt")
+
 
 def network_operations(job_id, _):
     print(f"{job_id}: Performing network operations")
